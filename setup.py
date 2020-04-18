71dcd6b0b13250195cf9e32acf43453dc1d2abd6
ChurakovMike <churakovmike@mail.ru>
2020-04-18 19:42:01 +0700

    + init setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='free-kassa-py',
+    description='FreeKassa python 3 client',
+    long_description='long_description',
+    long_description_content_type='text/markdown',
+    url='https://github.com/Churakovmike/free-kassa-py',
+    author='Mikhail Churakov',
+    email='churakovmike@mail.ru',
+
+    classifiers=[
+        'Development Status :: 5 - Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    keyword='freekassa',
+
+    packages=find_packages(exclude=['tests']),
+
+    install_requires=['requests'],
+
+    python_requires='>3.4',
+
+    project_urls={
+        'Source': 'https://github.com/Churakovmike/free-kassa-py'
+    }
+)
