c2e5e97769d77eaf6629b73d05b847ac804de3f8
ChurakovMike <churakovmike@mail.ru>
2020-04-19 23:15:49 +0700

    * update development status classifiers

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author_email='churakovmike@mail.ru',
 
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
 
