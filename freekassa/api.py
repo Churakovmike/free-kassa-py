320d150ec48d52bcba34699d60a3be99bb717814
ChurakovMike <churakovmike@mail.ru>
2020-04-20 00:02:27 +0700

    * add feature methods

--- freekassa/api.py
+++ freekassa/api.py
@@ -8,20 +8,25 @@ class FreeKassaApi:
     base_export_order_url = 'https://www.free-kassa.ru/export.php'
     wallet_api_url = 'https://www.fkwallet.ru/api_v1.php'
 
-    def __init__(self, merchant_id, first_secret, second_secret, wallet_id):
+    def __init__(self, merchant_id, first_secret, second_secret, wallet_id, wallet_api_key=''):
         self.merchant_id = merchant_id
         self.first_secret = first_secret
         self.second_secret = second_secret
         self.wallet_id = wallet_id
+        self.wallet_api_key = wallet_api_key
 
-    def send_request(self, params, method='post'):
+    def send_request(self, params, url=None, method='post'):
         """
         Send request to freekassa api
+        :param url:
         :param params:params
         :param method:method
         :return:
         """
-        return requests.__dict__[method](self.base_url, params=params)
+        if url is None:
+            url = self.base_url
+
+        return requests.__dict__[method](url, params=params)
 
     def get_balance(self):
         """
@@ -85,9 +90,102 @@ class FreeKassaApi:
 
         return self.send_request(params)
 
+    def get_wallet_balance(self):
+        """
+
+        :return:
+        """
+        params = {
+            'wallet_id': self.wallet_id,
+            'sign': self.generate_wallet_signature(),
+            'action': self.get_balance(),
+        }
+
+        return self.send_request(params=params, url=self.wallet_api_url)
+
+    def wallet_withdraw(self, purse, amount, currency, description, disable_exchange=1):
+        """
+        @todo: add generate tuple hash
+        :param purse:
+        :param amount:
+        :param currency:
+        :param description:
+        :param disable_exchange:
+        :return:
+        """
+        params = {
+            'wallet_id': self.wallet_id,
+            'purse': purse,
+            'amount': amount,
+            'desc': description,
+            'disable_exchange': disable_exchange,
+            'currency': currency,
+            'sign': '',
+            'action': 'cashout',
+        }
+
+        return self.send_request(params=params, url=self.wallet_api_url)
+
+    def get_operation_status(self, payment_id):
+        """
+        @todo: todo
+        :return:
+        """
+        pass
+
+    def transfer_money(self, purse, amount):
+        """
+        @todo: todo
+        :return:
+        """
+        pass
+
+    def online_payments(self, service_id, account, amount):
+        """
+        @todo: todo
+        :param service_id:
+        :param account:
+        :param amount:
+        :return:
+        """
+        pass
+
+    def get_online_services(self):
+        """
+        @todo: todo
+        :return:
+        """
+        pass
+
+    def get_online_payment_status(self, payment_id):
+        """
+        @todo: todo
+        :param payment_id:
+        :return:
+        """
+        pass
+
+    def generate_payment_link(self, order_id, summ, email='', description=''):
+        """
+        @todo: todo
+        :param order_id:
+        :param summ:
+        :param email:
+        :param description:
+        :return:
+        """
+        pass
+
     def generate_api_signature(self):
         """
         Generate api signature
         :return:str
         """
         return hashlib.md5(str(self.merchant_id).encode('utf-8') + str(self.second_secret).encode('utf-8')).hexdigest()
+
+    def generate_wallet_signature(self):
+        """
+        Generate wallet signature
+        :return:
+        """
+        return hashlib.md5(str(self.wallet_id + self.wallet_api_key).encode('utf-8')).hexdigest()
