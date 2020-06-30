85e172fcf9a8324b4d35a42de9ea99c016cd4450
ChurakovMike <churakovmike@mail.ru>
2020-06-30 15:43:55 +0700

    * update version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except FileNotFoundError:
 
 setup(
     name='free-kassa-py',
-    version='1.0.1',
+    version='1.0.2',
     description='FreeKassa python 3 client',
     long_description=description,
     long_description_content_type='text/markdown',
